167b50bb047c2edc883cfbd8bc33ff3ce63c34d2
thytran38 <ax4409h@gmail.com>
2021-04-29 22:40:15 +0700

    Init the VXR alternative project

---
+++ src/main/java/com/vexere/webservice/VexereApplication.java
@@ -0,0 +1,13 @@
+package com.vexere.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VexereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VexereApplication.class, args);
+	}
+
+}
