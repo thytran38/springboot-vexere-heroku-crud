dcd5739054a36c0e986ff1f37a7a3392b6b6b72e
thytran38 <ax4409h@gmail.com>
2021-05-01 02:29:38 +0700

    Add repos interfaces

--- src/main/java/com/vexere/webservice/repos/LocationRepository.java
+++ src/main/java/com/vexere/webservice/repos/LocationRepository.java
@@ -1,5 +1,6 @@
 package com.vexere.webservice.repos;
 
+import com.vexere.webservice.models.trips.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
